956557c22f22326aa1e3d04ab0a949260e7cafa4
Dynamite <damonjhastings@gmail.com>
2014-11-18 13:30:38 -0800

    Updates, Mobile Styles, Menu Styles

--- wp-content/plugins/social-media-icons-widget/lib/social-networks.php
+++ wp-content/plugins/social-media-icons-widget/lib/social-networks.php
@@ -14,8 +14,8 @@
 	The full-website-id should reflect the name of the image you create in each of the icon folder sizes, or in your custom icon directory. It is also used to populate the class field of the icon when the widget displays. The Social Icon Widget looks for .gif, .jpg, .jpeg, and .png in order and returns the first extention it finds.
 */
 
-	global $social_accounts;
-	$social_accounts = array(
+	global $siw_social_accounts;
+	$siw_social_accounts = array(
 		'500px' => 'fivehundredpx',
 		'About.me' => 'aboutme',
 		'Behance' => 'behance',
@@ -36,6 +36,7 @@
 		'Path' => 'path',
 		'Pinterest' => 'pinterest',
 		'RSS Feed' => 'rss',
+		'SoundCloud' => 'soundcloud',
 		'Speaker Deck' => 'speakerdeck',
 		'StumbleUpon' => 'stumbleupon',
 		'Technorati' => 'technorati',
@@ -50,6 +51,6 @@
 	);
 
 	if( has_filter('social_icon_accounts') ) {
-		$social_accounts = apply_filters('social_icon_accounts', $social_accounts);
+		$siw_social_accounts = apply_filters('social_icon_accounts', $siw_social_accounts);
 	}
 ?>
--- wp-content/themes/rmh/block/layout/header.php
+++ wp-content/themes/rmh/block/layout/header.php
@@ -1,6 +1,7 @@
 <?php $header_image = get_header_image(); ?>
 <header>
 	<div class="container">
+		<div class="mobile menu-button"></div>
 		<div class="site-logo">
 			<a href="<?php echo home_url();?>">
 <?php 	if ( ! empty( $header_image ) ) { ?>
@@ -14,8 +15,10 @@
 			<?php dynamic_sidebar('Social Icons');?>
 			</ul>	
 		</div> 
+		<div class="mobile search-button"></div>
 	</div>
-	<div class="container">
+	<div class="main-menu container">
 		<?php wp_nav_menu( array( 'menu' => 'header-nav') ); ?>	
 	</div>
 </header>
+
