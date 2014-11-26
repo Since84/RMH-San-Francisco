099b2ad2611386851535c2b4e2ccf32afd0338bd
Dynamite <damonjhastings@gmail.com>
2014-11-26 12:07:44 -0800

    Style Refactor

---
+++ wp-content/themes/ronald-house/home.php
@@ -0,0 +1,64 @@
+<?php //Home 
+
+	Boom_Util::get_template_parts(
+		array(
+//HEADER
+			'block/layout/html-header' 
+			,'block/layout/header'
+
+//SLIDER
+			,'block/component/slider'
+
+//CALL TO ACTION
+			,'block/component/call-to-action'
+		));
+
+//WHAT'S HAPPENING
+?>
+<div class="content-area container">
+	<div class="whats-happening">
+		<h2 class="">What's Happening</h2>
+<?php
+		Boom_Util::get_template_parts(
+			array(
+				'components/post-scroller/four-per-page'
+			));
+?>
+	</div>
+	<div class="lower-content">
+
+<? //FAMILY STORIES ?>
+		<div class="family-stories">
+			<h2>Family Stories</h2>
+<?php
+		Boom_Util::get_template_parts(
+			array(
+				'components/post-scroller/one-per-page'
+			)
+		);
+?>
+		</div>
+
+
+<? //SOCIAL	?>
+		<div class="social-block">
+			<h2>Social</h2>
+<?php
+//FAMILY STORIES
+		Boom_Util::get_template_parts(
+			array(
+				'block/content/facebook-social'
+			)
+		);
+?>		
+		</div>
+	</div>
+	</div>
+<?php
+//FOOTER
+		Boom_Util::get_template_parts(
+			array(
+				'block/layout/footer'
+				,'block/layout/html-footer'
+			)
+		);
