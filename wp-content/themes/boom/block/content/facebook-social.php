02cd30fe91d995dc2e6f1704e6268f16cc7b636a
Damon <damonjhastins@gmail.com>
2015-02-18 14:29:03 -0800

    Facebook Widget Height

--- wp-content/themes/boom/block/content/facebook-social.php
+++ wp-content/themes/boom/block/content/facebook-social.php
@@ -3,7 +3,7 @@
 <iframe 
 src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook
 .com%2Fpages%2FThe-San-Francisco-Ronald-McDonald-House%2F107760955920998&am
-p;width=320&amp;height=590&amp;colorscheme=light&amp;show_faces=true&amp;he
+p;width=320&amp;height=350&amp;colorscheme=light&amp;show_faces=true&amp;he
 ader=true&amp;stream=true&amp;show_border=true" scrolling="no"
 frameborder="0" style="border:none; overflow:hidden; width:320px;
-height:590px;" allowTransparency="true"></iframe>
+height:350px;" allowTransparency="true"></iframe>
